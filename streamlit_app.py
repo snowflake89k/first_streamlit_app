8a9abb9ef4a8bc5d554681e33decf3c1ec344796
snowflake89k <126277806+snowflake89k@users.noreply.github.com>
2023-02-28 11:30:12 +0530

    Update streamlit_app.py
    added dataframe to extract data from a csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 streamlit.title('My Parents Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3')
 streamlit.text('🥑🍞Avocado toast')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") "dataframe"
+streamlit.dataframe(my_fruit_list)
