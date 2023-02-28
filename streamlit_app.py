a252ef64008672bd1001ca6625e13cac6604c648
snowflake89k <126277806+snowflake89k@users.noreply.github.com>
2023-02-28 11:08:20 +0530

    Update streamlit_app.py
    Added the Header and text fields

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My first code in streamlit')
+streamlit.header('My first code in streamlit - Header')
+streamlit.text('My first code in streamlit - Text1')
+streamlit.text('My first code in streamlit - Text2')
