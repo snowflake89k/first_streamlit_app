ddfc354a6bbf63c074413bf56714bfb0e73f4158
snowflake89k <126277806+snowflake89k@users.noreply.github.com>
2023-03-07 18:10:01 +0530

    Update streamlit_app.py
    Set Fruit as an Index from drop down options

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3')
 streamlit.text('🥑🍞Avocado toast')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+my_fruit_list = my_fruit_list.set_index('Fruit') 
 
 # code a drop-down list
 streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
