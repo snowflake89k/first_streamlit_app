af22201d1b3b0453a1eb1c2cf70c00f3e6e4a880
snowflake89k <126277806+snowflake89k@users.noreply.github.com>
2023-03-07 18:05:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ streamlit.text('🥑🍞Avocado toast')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 
 # code a drop-down list
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.fruit))
 
 streamlit.dataframe(my_fruit_list)
