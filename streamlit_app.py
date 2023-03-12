d9b1c30b8cbf09d718d9197833495e72bde9a592
snowflake89k <126277806+snowflake89k@users.noreply.github.com>
2023-03-12 11:47:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 import pandas
-import snowflake.connector
 import requests
+import snowflake.connector
+from urllib.error import URLError
+
 
 streamlit.title('My Parents Healthy Diner')
 streamlit.header('Breakfast Favourites')
@@ -12,32 +14,38 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # code a drop-down list
 my_fruit_list = my_fruit_list.set_index('Fruit') 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
+# ----------------------------------------------------------------------------------------------------------------------#
+# alternatives
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 #streamlit.dataframe(my_fruit_list)
 #streamlit.dataframe(fruits_selected)
-streamlit.dataframe(fruits_to_show)
+# ----------------------------------------------------------------------------------------------------------------------#
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice fruit advice!')
 
-
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
-
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
+# import requests
 
 fruit_choice = streamlit.text_input('What fruit would you like information about')
 streamlit.write('The user entered', fruit_choice)
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruitvice_normalized)
 
+# ----------------------------------------------------------------------------------------------------------------------#
+# alternatives
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# ----------------------------------------------------------------------------------------------------------------------#
+#do not run anything beyond this line.
+streamlit.stop()
 
+
+# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
